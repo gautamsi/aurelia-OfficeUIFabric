4f53a3f2b078b7e44b44045843add33efc646881
Gautam Singh <mailtosinghs@gmail.com>
2016-02-18 16:22:21 +0530

    spinner update

--- src/Spinner/Spinner.ts
+++ src/Spinner/Spinner.ts
@@ -4,16 +4,18 @@ import * as Fabric from './SpinnerComponent';
 //@inject(Element)
 @customElement('office-spinner')
 export class Spinner {
-    @bindable text:string = "Loading...";
-    @bindable hasText:boolean = false;
-    @bindable large:boolean = false;
+    @bindable text:string;
+    @bindable showText:boolean = false;
+    @bindable size:boolean = false;
     
-    element:Element;
+    defaultText:string = "Loading...";
+    $spinner:Element;
     constructor()
     {                
     }
     attached(){
-        var spinned = Fabric.Spinner(this.element);
+        var spinned = Fabric.Spinner(this.$spinner);
+        this.text = this.text || this.defaultText;
     }
 }
 
